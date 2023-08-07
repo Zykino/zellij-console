9d7403e0a8d0068906eab3cf6eddb3540961170e
Zykino <Zykino@users.noreply.github.com>
2023-08-07 23:33:43 +0200

    Copy `monocle`’s plugin to have a POC for `action run`

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,142 @@
-use ansi_term::{Style, Colour::Fixed};
+mod ui;
+
 use zellij_tile::prelude::*;
 
-use std::collections::HashMap;
-use std::path::PathBuf;
+use serde::{Deserialize, Serialize};
 
 #[derive(Default)]
 struct State {
-    mode_log: HashMap<String, usize>,
-    tabs: Vec<String>,
-    test_runs: usize,
+    action: String,
+    // file_name_search_results: Vec<String>,
+    // file_contents_search_results: Vec<String>,
+    loading: bool,
+    loading_animation_offset: u8,
+    should_open_floating: bool,
+    search_filter: EnvironmentFrom,
+    display_rows: usize,
+    display_columns: usize,
+    displayed_search_results: (usize, Vec<String>), // usize is selected index
+
+    last_pane: PaneManifest,
+    last_tab: TabInfo, // TODO: useless?
 }
 
 register_plugin!(State);
 
 impl ZellijPlugin for State {
     fn load(&mut self) {
-        subscribe(&[EventType::ModeUpdate, EventType::TabUpdate, EventType::Key]);
+        subscribe(&[EventType::PaneUpdate, EventType::TabUpdate, EventType::Key]);
     }
 
     fn update(&mut self, event: Event) -> bool {
         let mut should_render = false;
+
         match event {
-            Event::ModeUpdate(mode_info) => {
-                let mode = format!("{:?}", mode_info.mode);
-                let count = self.mode_log.entry(mode).or_insert(0);
-                *count += 1;
-                should_render = true;
+            Event::PaneUpdate(pane_info) => {
+                self.last_pane = pane_info;
+                // should_render = true;
             }
             Event::TabUpdate(tab_info) => {
-                self.tabs = tab_info.iter().map(|t| t.name.clone()).collect();
-                should_render = true;
+                self.last_tab = tab_info.iter().find(|t| t.active).unwrap().clone();
+                // should_render = true;
             }
             Event::Key(key) => {
-                if let Key::Char('n') = key {
-                    self.test_runs += 1;
-                    open_command_pane_floating("cargo", vec!["test"]);
-                }
+                self.handle_key(key);
+                should_render = true;
             }
-            _ => (),
+            _ => unimplemented!(),
         };
+
         should_render
     }
 
     fn render(&mut self, rows: usize, cols: usize) {
-        let colored_rows = color_bold(CYAN, &rows.to_string());
-        let colored_cols = color_bold(CYAN, &cols.to_string());
-        println!("");
-        println!("I have {} rows and {} columns", colored_rows, colored_cols);
-        println!("");
-        println!("{}", color_bold(GREEN, "Modes:"));
-        for (mode, count) in &self.mode_log {
-            let count = color_bold(ORANGE, &count.to_string());
-            println!("{} -> Changed {} times", mode, count);
+        self.change_size(rows, cols);
+        print!("{}", self);
+    }
+}
+
+impl State {
+    pub fn handle_key(&mut self, key: Key) {
+        match key {
+            // Key::Down => self.move_search_selection_down(),
+            // Key::Up => self.move_search_selection_up(),
+            Key::Char('\n') => self.start_action(),
+            // Key::BackTab => self.open_search_result_in_terminal(),
+            Key::Ctrl('f') => {
+                self.should_open_floating = !self.should_open_floating;
+            }
+            Key::Ctrl('r') => self.search_filter.progress(),
+            // Key::Esc | Key::Ctrl('c') => {
+            //     if !self.search_term.is_empty() {
+            //         self.clear_state();
+            //     } else {
+            //         hide_self();
+            //     }
+            // }
+            _ => self.append_to_search_term(key),
         }
-        println!("");
-        let current_tabs = color_bold(GREEN, "Current Tabs:");
-        let comma = color_bold(ORANGE, ", ");
-        println!("{} {}", current_tabs, self.tabs.join(&comma));
-        println!("");
-        if self.test_runs > 0 {
-            let test_run_count = color_bold(CYAN, &self.test_runs.to_string());
-            println!("Ran tests {} times!", test_run_count);
+    }
+
+    pub fn change_size(&mut self, rows: usize, cols: usize) {
+        self.display_rows = rows;
+        self.display_columns = cols;
+    }
+
+    fn append_to_search_term(&mut self, key: Key) {
+        match key {
+            Key::Char(character) => {
+                self.action.push(character);
+            }
+            Key::Backspace => {
+                self.action.pop();
+                if self.action.len() == 0 {
+                    // self.clear_state();
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn start_action(&mut self) {
+        let (cmd, args) = match self.search_filter {
+            EnvironmentFrom::ZellijSession => ("env", Vec::<&str>::new()),
+            EnvironmentFrom::DefaultShell => ("fish", vec!["-c", "env"]),
+            EnvironmentFrom::LastPane => todo!(),
+        };
+
+        match self.action.as_str() {
+            "run" => {
+                if self.should_open_floating {
+                    open_command_pane_floating(cmd, args);
+                } else {
+                    open_command_pane(cmd, args);
+                }
+            }
+            _ => (),
         }
     }
 }
 
-pub const CYAN: u8 = 51;
-pub const GRAY_LIGHT: u8 = 238;
-pub const GRAY_DARK: u8 = 245;
-pub const WHITE: u8 = 15;
-pub const BLACK: u8 = 16;
-pub const RED: u8 = 124;
-pub const GREEN: u8 = 154;
-pub const ORANGE: u8 = 166;
+#[derive(Serialize, Deserialize)]
+pub enum EnvironmentFrom {
+    ZellijSession,
+    DefaultShell,
+    LastPane,
+}
 
-fn color_bold(color: u8, text: &str) -> String {
-    format!("{}", Style::new().fg(Fixed(color)).bold().paint(text))
+impl EnvironmentFrom {
+    pub fn progress(&mut self) {
+        match &self {
+            &EnvironmentFrom::ZellijSession => *self = EnvironmentFrom::DefaultShell,
+            &EnvironmentFrom::DefaultShell => *self = EnvironmentFrom::LastPane,
+            &EnvironmentFrom::LastPane => *self = EnvironmentFrom::ZellijSession,
+        }
+    }
+}
+
+impl Default for EnvironmentFrom {
+    fn default() -> Self {
+        EnvironmentFrom::ZellijSession
+    }
 }
